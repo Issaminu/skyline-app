6ea1e0722039bc299653cd7143bf84abe136b44d
Issam Boubcher <boubcher.issam@gmail.com>
2022-05-10 20:58:41 +0100

    fix attempt

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,13 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { useUser } from '@auth0/nextjs-auth0'
+import Link from 'next/link'
 export default function Home() {
   const { user, isLoading, error } = useUser();
   return (
     <div className={styles.container}>
       {user ? (<>
-        <p>{console.log(user)}</p>
-        <a href="/api/auth/logout">Logout</a> </>) : <a href="/api/auth/login">Login</a>}
+        <Link><a href="/api/auth/logout">Logout</a></Link> </>) : <Link><a href="/api/auth/login">Login</a></Link>}
     </div>
   )
 }
