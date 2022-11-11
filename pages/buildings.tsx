1d594473058a37fd35d73a5eec0450cc14c6d817
Issam-Boubcher <99417396+Issam-Boubcher@users.noreply.github.com>
2022-11-11 02:42:09 -0800

    testing codespaces

--- pages/buildings.tsx
+++ pages/buildings.tsx
@@ -227,7 +227,7 @@ const Buildings = (props) => {
       <main className="flex-1 pb-8 mt-4 sm:mt-4 md:mt-52 lg:mt-56">
         <div className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
           <h2 className="text-lg leading-6 font-medium text-gray-900">
-            Overview
+            Overvieww
           </h2>
           <div className="mt-2 grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3">
             {/* Card */}
