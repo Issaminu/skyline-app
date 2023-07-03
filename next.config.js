0ffe83d564eee9847a4f0946991ef4f44b43762c
Issam Boubcher <99417396+Issaminu@users.noreply.github.com>
2023-07-03 21:00:05 +0100

    Reset `new-skyline`

--- next.config.js
+++ next.config.js
@@ -1,29 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    domains: [
-      "tailwindui.com",
-      "images.unsplash.com",
-      "skyline-app-bucket.s3.eu-west-3.amazonaws.com",
-    ],
-  },
-  webpack: (config, options) => {
-    config.experiments = {
-      topLevelAwait: true,
-      layers: true,
-    };
-    return config;
-  },
-};
-const intercept = require("intercept-stdout");
-function interceptStdout(text) {
-  if (text.includes("Duplicate atom key")) {
-    return "";
-  }
-  return text;
-}
-intercept(interceptStdout);
+const nextConfig = {}
 
-module.exports = nextConfig;
+module.exports = nextConfig
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,18 @@
 /** @type {import('tailwindcss').Config} */
-
-const colors = require("tailwindcss/colors");
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      fontFamily: {
-        sans: ["Inter"],
-      },
-      colors: {
-        cyan: colors.cyan,
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
-  plugins: [
-    require("@tailwindcss/forms"),
-    require("@tailwindcss/aspect-ratio"),
-  ],
-};
+  plugins: [],
+}
