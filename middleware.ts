a9f8c15cb1e1d659c33f0822b02d3f03d83e078c
Issam Boubcher <99417396+Issaminu@users.noreply.github.com>
2023-08-30 15:06:52 +0100

    Another `title` fix

--- middleware.ts
+++ middleware.ts
@@ -5,19 +5,21 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
   afterAuth(auth, req, evt) {
     const requestHeaders = new Headers(req.headers);
-    requestHeaders.set("x-url", req.url);
     const pathname = req.nextUrl.pathname;
+    requestHeaders.set("x-url", pathname);
     const isRoutePublic = routeIsPublic(pathname);
     if (!auth.userId && !isRoutePublic) {
       return NextResponse.redirect(
-        `${new URL("/login", req.url)}?redirect_url=${pathname.slice(1)}`,
+        `${new URL("/login", req.url)}?redirect_url=${encodeURIComponent(
+          req.url
+        )}`,
         {
           headers: requestHeaders,
         }
       );
     }
     if (auth.userId && isRoutePublic) {
-      return NextResponse.redirect(new URL("/immeubles", req.url), {
+      return NextResponse.redirect(new URL("/buildings", req.url), {
         headers: requestHeaders,
       });
     }
